5fd29c2698f540d08bcbea3f492ea80f3434c5cd
Jeremy Reimer <jeremy.reimer@gmail.com>
2024-08-26 12:55:02 -0700

    Added README.

---
+++ README.md
@@ -0,0 +1,42 @@
+# GalaxyEngine - A simple 3D game engine using LISP as its scripting language
+
+![image](galaxy-engine-logo.jpg)
+
+## Introduction
+
+GalaxyEngine is an open source 3D game engine that uses LISP as its native scripting language.
+
+The purpose of GalaxyEngine is to allow development of simple 3D games using an easy-to-use integrated development environment where everything is easily customizable.
+
+## Building and running GalaxyEngine
+
+Right now, GalaxyEngine only runs on macOS. However, the code is portable across three platforms: macOS, Windows, and Linux.
+
+This is because GalaxyEngine is written in C++ and uses the Vulkan 3D graphics API and the glfw windowing library. Both of these are available across all three platforms.
+
+Stay tuned for updates as they become available!
+
+### Requirements
+
+* Currently, an Apple computer running macOS
+* The Vulkan 3D SDK
+* The glfw windowing library
+* The glm matrix math library
+
+### Building
+
+* Stay tuned!
+
+### Running
+
+* Stay tuned!
+
+## Using GalaxyEngine
+
+GalaxyEngine is a work in progress. Please check back for further updates.
+
+## Version
+
+Current version: 0.02 August 26, 2024 - Initializes blank glfw window at 1200 x 600 and initializes Vulkan layer. That's it!
+
+
--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -1,14 +1,118 @@
-//
-//  main.cpp
-//  VulkanTest
-//
-//  Created by Jeremy Reimer on 2024-08-22.
-//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const uint32_t WIDTH = 1200;
+const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+    VkInstance instance;
+    
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Galaxy Engine 0.02", nullptr, nullptr);
+    }
+    
+    void initVulkan() {
+        createInstance();
+    }
+    
+    std::vector<const char*> getRequiredExtensions(){
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        memcpy(extensions.data(), glfwExtensions, sizeof(const char*) * glfwExtensionCount);
+        extensions.push_back("VK_KHR_portability_enumeration");
+        return extensions;
+    }
+    
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
+        
+        //FOR WINDOWS, UNCOMMENT BELOW AND COMMENT SIMILAR STUFF ABOVE
+        //See: https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance
+        //uint32_t glfwExtensionCount = 0;
+        //const char** glfwExtensions;
+        
+        //glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        //createInfo.enabledExtensionCount = glfwExtensionCount;
+        //createInfo.ppEnabledExtensionNames = glfwExtensions;
+        
+        createInfo.enabledLayerCount = 0;
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance!");
+        }
+
+    }
+    
+
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
